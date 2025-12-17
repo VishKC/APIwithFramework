11e8196fd70792fa5b9402dd716613e081007136
Vish KC <visuskc@gmail.com>
2025-12-17 20:22:19 +0530

    update file reading and pom
    Updated the logic to read 'global.properties' file and pom.xml with report dependencies..

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -1,13 +1,11 @@
 package resources;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Properties;
 
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
@@ -19,40 +17,36 @@ import io.restassured.specification.RequestSpecification;
 public class Utils {
 
 	public static RequestSpecification req;
-	public RequestSpecification requestSpecification() throws IOException
-	{
-		
-		if(req==null)
-		{
-		PrintStream log =new PrintStream(new FileOutputStream("logging.txt"));
-		 req=new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl")).addQueryParam("key", "qaclick123")
-				 .addFilter(RequestLoggingFilter.logRequestTo(log))
-				 .addFilter(ResponseLoggingFilter.logResponseTo(log))
-		.setContentType(ContentType.JSON).build();
-		 return req;
+
+	public RequestSpecification requestSpecification() throws IOException {
+
+		if (req == null) {
+			PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
+			req = new RequestSpecBuilder().setBaseUri(getGlobalValue("baseUrl")).addQueryParam("key", "qaclick123")
+					.addFilter(RequestLoggingFilter.logRequestTo(log))
+					.addFilter(ResponseLoggingFilter.logResponseTo(log)).setContentType(ContentType.JSON).build();
+			return req;
 		}
 		return req;
-		
-		
+
 	}
-	
-	
-	public static String getGlobalValue(String key) throws IOException
-	{
-		Properties prop =new Properties();
-		FileInputStream fis =new FileInputStream("/Users/rahulshetty/restassurednew/APIFramework/src/test/java/resources/global.properties");
+
+	public static String getGlobalValue(String key) throws IOException {
+		Properties prop = new Properties();
+//		FileInputStream fis =new FileInputStream("/Users/User/WorkSpace/APIwithFramework/src/test/java/resources/global.properties");
+
+		String projectPath = System.getProperty("user.dir");
+		String FilePath = projectPath + "/src/test/java/resources/global.properties";
+		FileInputStream fis = new FileInputStream(FilePath);
+
 		prop.load(fis);
 		return prop.getProperty(key);
-	
-		
-		
+
 	}
-	
-	
-	public String getJsonPath(Response response,String key)
-	{
-		  String resp=response.asString();
-		JsonPath   js = new JsonPath(resp);
+
+	public String getJsonPath(Response response, String key) {
+		String resp = response.asString();
+		JsonPath js = new JsonPath(resp);
 		return js.get(key).toString();
 	}
 }
